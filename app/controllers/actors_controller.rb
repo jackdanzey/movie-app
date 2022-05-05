c6042513d758b8caf35e9e8b044a5fa1a26eff68
Jack Danzey <jackdanzey@gmail.com>
2022-05-05 08:29:00 -0400

    adds route and one_actor method

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,6 @@
 class ActorsController < ApplicationController
+  def one_actor
+    actor = Actor.find_by(id: params["id"])
+    render json: actor.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/one_actor" => "actors#one_actor"
+  get "/one_actor/:id" => "actors#one_actor"
 end
