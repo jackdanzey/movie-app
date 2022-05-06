0a8c3c8241e631dc6cb333177d8b4665732e2a55
Jack Danzey <jackdanzey@gmail.com>
2022-05-06 08:29:37 -0400

    Adds movie model and content to seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 actor = Actor.create({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+movie = Movie.create({ title: "Memento", year: 2000, plot: "Normal guy just wants to find his wife's killer" })
+movie = Movie.create({ title: "Seven", year: 1995, plot: "Two detectives try to solve a string of murders" })
+movie = Movie.create({ title: "The Sting", year: 1973, plot: "Two con artists swindle a bad guy out of his money" })
